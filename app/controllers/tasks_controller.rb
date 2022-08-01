8bc4ea1588b782e6abbd4583cea6a30f8d870fef
Takahiro <deaman6948@gmail.com>
2022-08-01 15:07:56 +0000

    C5終了

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,16 +1,27 @@
 class TasksController < ApplicationController
    before_action :set_user
+
    
   
    def index
     @tasks = @user.tasks
+    @task = Task.all
+  
    end
-  
-  
+   
+   def show
+
+   end
+   
+  def destroy
+    
+  end
+
   private
   
    def set_user
       @user = User.find(params[:user_id])
+     
    end
-  
+   
 end
