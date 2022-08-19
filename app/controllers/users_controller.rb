eba278e3b50097755bba7e5ea10e05a81d52a2e8
Takahiro <deaman6948@gmail.com>
2022-08-19 04:26:06 +0000

    E4完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :show, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: :destroy
+  before_action :admin_or_correct_user, only[:edit, :update, :destroy]
  
   def new
     @user = User.new
@@ -69,6 +70,14 @@ class UsersController < ApplicationController
    
     def admin_user
        redirect_to root_url unless current_user.admin?
+    end
+    
+    def admin_or_correct_user
+      @user = User.find(params[:id]) if @user.blank?
+      unless current_user?(@user) || current_user.admin?
+        flash[:danger]= "編集権限がありません"
+        redirect_to(root_url)
+      end
     end
     
 end
