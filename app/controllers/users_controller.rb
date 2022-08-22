60bb3e0567605488103259fdadcc4ecc51e74ddd
Takahiro <deaman6948@gmail.com>
2022-08-22 13:26:06 +0000

    F13完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i( show edit update destroy)
-  before_action :logged_in_user, only: [:index, :show, :edit, :update, :destroy]
-  before_action :correct_user, only: [:edit, :update]
-  before_action :admin_user, only: [:destroy, :index]
-  before_action :admin_or_correct_user, only[:edit, :update, :destroy]
+  before_action :logged_in_user, only: %i(index show edit update destroy)
+  before_action :correct_user, only:  %i(edit update)
+  before_action :admin_user, only: %i(destroy index)
+  before_action :admin_or_correct_user, only: %i(edit update destroy)
  
   def new
     @user = User.new
